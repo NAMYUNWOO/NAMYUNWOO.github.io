a73f169943d39276ce72f1443851b3055fb9b139
namyunwoo <namyunwoo@icloud.com>
2025-01-25 20:40:51 +0900

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+module.exports = {
+  assetPrefix: isProd ? '/NAMYUNWOO.github.io/' : '',
+  basePath: isProd ? '/NAMYUNWOO.github.io' : '',
+  trailingSlash: true,
+}
 
-export default nextConfig;
+
